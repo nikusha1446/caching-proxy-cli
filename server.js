b4a68f0d8dd6b9b3813d8d9bed6e2be99706b197
nikusha1446 <nikusha1446@gmail.com>
2025-09-25 00:49:27 +0400

    feat: add caching functionality with NodeCache

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import express from 'express';
+import NodeCache from 'node-cache';
+
+const cache = new NodeCache();
 
 function createServer(port, origin) {
   const app = express();
@@ -8,6 +11,14 @@ function createServer(port, origin) {
 
   app.use(async (req, res) => {
     const cleanOrigin = origin.endsWith('/') ? origin.slice(0, -1) : origin;
+
+    const cacheKey = `${req.method}:${req.originalUrl}`;
+    const cachedResponse = cache.get(cacheKey);
+
+    if(cachedResponse) {
+      res.set('X-Cache', 'HIT');
+      return res.status(cachedResponse.status).json(cachedResponse.data);
+    }
 
     try {
       const response = await axios({
@@ -20,6 +31,11 @@ function createServer(port, origin) {
           'accept-encoding': undefined,
         },
         validateStatus: (status) => status < 400,
+      });
+
+      cache.set(cacheKey, {
+        status: response.status,
+        data: response.data
       });
 
       res.set('X-Cache', 'MISS');
